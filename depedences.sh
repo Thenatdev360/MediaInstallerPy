60219c2aa8aacc2a36553120a6f07879a5517f8b
Thenatdev360 <soynatpro@gmail.com>
2026-02-12 00:34:57 -0600

    Refactor dependency installation script
    I did the same change but in linux i'll waiting someone enjoy this program <3

--- depedences.sh
+++ depedences.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-echo "MediaInstaller Setup"
+echo "MediaInstaller Dependencies"
 
-if ! command -v python3 &> /dev/null
-then
-    echo "Installing Python..."
+# PYTHON
+if ! command -v python3 &> /dev/null; then
     sudo apt install python3 -y || brew install python
 fi
 
-mkdir -p media
-
-if ! command -v yt-dlp &> /dev/null
-then
-    sudo apt install yt-dlp ffmpeg -y || brew install yt-dlp ffmpeg
+# NODE
+if ! command -v node &> /dev/null; then
+    sudo apt install nodejs npm -y || brew install node
 fi
+
+# yt-dlp
+if [ ! -f yt-dlp ]; then
+    curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+    chmod +x yt-dlp
+fi
+
+mkdir -p media
 
 echo "DONE"

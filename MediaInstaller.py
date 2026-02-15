e54f4883ec7dc3e0f431ab036759a464899184a6
Thenatdev360 <soynatpro@gmail.com>
2026-02-14 23:39:31 -0600

    Add function run_with_cookies
    this is a new function and run the commands with cookies.txt and test if work or not work

--- MediaInstaller.py
+++ MediaInstaller.py
@@ -1,7 +1,10 @@
 import subprocess
 import os
+import platform
 
 MEDIA_DIR = "media"
+COOKIES_FILE = "cookies.txt"
+
 os.makedirs(MEDIA_DIR, exist_ok=True)
 
 browsers = ["edge", "chrome", "firefox"]
@@ -22,9 +25,11 @@ BASE = [
     "--remote-components", "ejs:github"
 ]
 
-def run_with_browser(browser):
-    print(f"Trying {browser} cookies...")
-    cmd = BASE + ["--cookies-from-browser", browser]
+def is_android():
+    return "android" in platform.platform().lower()
+
+def build_cmd(extra):
+    cmd = BASE + extra
 
     if option == "1":
         os.makedirs(f"{MEDIA_DIR}/music", exist_ok=True)
@@ -35,32 +40,46 @@ def run_with_browser(browser):
             "--embed-metadata",
             "--write-subs",
             "--write-auto-subs",
-            "--sub-lang", "en,es",
+            "--sub-lang", "en,es", 
             "-o", f"{MEDIA_DIR}/music/%(title)s.%(ext)s",
             url
         ]
+
     else:
         os.makedirs(f"{MEDIA_DIR}/video", exist_ok=True)
         cmd += [
-            "-f", "bv*+ba/b",
+            "-f", "bestvideo*+bestaudio/best",
             "--merge-output-format", "mp4",
             "--write-subs",
             "--write-auto-subs",
-            "--sub-lang", "en,es",
+            "--sub-lang", "en,es", 
             "-o", f"{MEDIA_DIR}/video/%(title)s.%(ext)s",
             url
         ]
 
     return subprocess.run(cmd).returncode == 0
 
+def run_with_cookies():
+    if os.path.exists(COOKIES_FILE):
+        print("Trying cookies.txt...")
+        return build_cmd(["--cookies", COOKIES_FILE])
+    return False
+
+def run_with_browser(browser):
+    print(f"Trying {browser}...")
+    return build_cmd(["--cookies-from-browser", browser])
+
 success = False
 
-for b in browsers:
-    if run_with_browser(b):
-        success = True
-        break
+success = run_with_cookies()
 
-if not success:
-    print("\n Sorry all browsers failed.")
+if not success and not is_android():
+    for b in browsers:
+        if run_with_browser(b):
+            success = True
+            break
+
+if success:
+    print("\nDownloaded successfully.")
 else:
-    print("\n Downloaded successfully.")
+    print("\nAll methods failed.")
